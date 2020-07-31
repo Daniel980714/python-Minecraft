cf15e1cf9daaf5f46bf100593b81ee39b8b89566
Daniel980714 <68846466+Daniel980714@users.noreply.github.com>
2020-07-31 11:44:27 +0800

    Add files via upload

--- 5-1.py
+++ 5-1.py
@@ -7,8 +7,8 @@ This is a temporary script file.
 
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
-import time
 x,y,z = mc.player.getTilePos()
 for i in range(30):
-    mc.setBlocks(x+i,y-i,z+i,x-i,y-i,z-i,24)
-    time.sleep (0.2)
+    mc.setBlocks(x+i,y-i-1,z+i,x-i,y-i-1,z-i,24)
+for j in range(28):
+    mc.setBlocks(x+j,y-j-2,z+j,x-j,y-j-2,z-j,0)
